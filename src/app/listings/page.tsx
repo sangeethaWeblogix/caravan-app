8a6b5db3caa5d26d09c63e15c2b86e8e9d2e0131
pavi <pavi.s@weblogix.com.au>
2025-06-24 17:05:03 +0530

    small changes in list page

--- src/app/listings/page.tsx
+++ src/app/listings/page.tsx
@@ -1,4 +1,4 @@
- 'use client';
+  'use client';
 import { useEffect, useState } from 'react';
 import { fetchListings } from '../../api/listings/api'; // your custom API call
 import Image from 'next/image';
@@ -29,8 +29,6 @@ interface Pagination {
 
 export default function ListingsPage() {
  const [products, setProducts] = useState<Product[]>([]);
- const [totalCount, setTotalCount] = useState<number>(0);
-
   const [pagination, setPagination] = useState<Pagination>({
     current_page: 1,
     total_pages: 1,
@@ -44,8 +42,7 @@ const loadListings = async (page = 1) => {
 
     if (response?.data?.products && response?.pagination) {
       setProducts(response.data.products);
-      setPagination(response.pagination); 
-        setTotalCount(response.data.total_products || response.data.products.length); // fallback
+      setPagination(response.pagination); // ✅ Use actual values from backend
     } else {
       console.warn('Unexpected response shape:', response);
       setProducts([]);
@@ -61,7 +58,8 @@ const loadListings = async (page = 1) => {
 };
 
 
-   useEffect(() => {
+  console.log("Products:", products);
+  useEffect(() => {
     loadListings();
   }, []);
 
@@ -85,16 +83,16 @@ const handlePrevPage = () => {
      <section className="services section-padding pt-30 pb-30 style-1">
       <div className="container">
         <div className="content">
-          <div className="row justify-content-center">
+          <h2 className='pt-30 pb-30'>6585 Caravans For Sale in Australia</h2>
+          <div className="row justify-content-center mt-8">
             <div className="col-lg-3 col-12 col-md-4">
               <div className="filter">
               <CaravanFilter />
               </div>
               </div>
-              <h1></h1>
 <Lisiting
   products={products}
-   pagination={pagination}
+  pagination={pagination}
   onNext={handleNextPage}
   onPrev={handlePrevPage}
 />          
