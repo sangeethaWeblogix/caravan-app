d406d2f867ec1f0acf17af33b01a74919a2d776c
pavi <pavi.s@weblogix.com.au>
2025-07-21 14:04:21 +0530

    og tag test 2

--- src/app/listings/[...Filters]/page copy.tsx
+++ src/app/listings/[...Filters]/page copy.tsx
@@ -1,10 +1,9 @@
-// app/listings/page.tsx
-import React, { Suspense } from "react";
-import Listing from "../components/ListContent/Listings"; // Path to your Listing component
+import { usePathname } from "next/navigation";
+import ListingsPage from "@/app/components/ListContent/Listings";
 import { fetchListings } from "@/api/listings/api";
 import { Metadata } from "next";
 
-// This function will run server-side to dynamically generate metadata
+// Modify the `generateMetadata` function to fetch and return metadata
 export async function generateMetadata(): Promise<Metadata> {
   const imageUrl = "public/favicon.ico";
 
@@ -46,20 +45,35 @@ export async function generateMetadata(): Promise<Metadata> {
   };
 }
 
-// Main component to render the page content
-
-const ListingsPage = ({
+// Listings component now accepts metaTitle and metaDescription as props
+const Listings = ({
   metaTitle,
   metaDescription,
 }: {
   metaTitle: string;
   metaDescription: string;
 }) => {
+  const pathname = usePathname();
+
+  const pathParts = pathname?.split("/").filter(Boolean);
+  const slug1 = pathParts?.[1];
+  const slug2 = pathParts?.[2];
+
+  const category = slug1?.endsWith("-category")
+    ? slug1.replace("-category", "")
+    : undefined;
+  const location = (slug2 ?? (slug1?.endsWith("-state") ? slug1 : undefined))
+    ?.replace("-state", "")
+    ?.replace(/-/g, " ");
+
   return (
-    <Suspense fallback={<div>Loading listings...</div>}>
-      <Listing metaTitle={metaTitle} metaDescription={metaDescription} />
-    </Suspense>
+    <ListingsPage
+      category={category}
+      location={location}
+      metaTitle={metaTitle}
+      metaDescription={metaDescription}
+    />
   );
 };
 
-export default ListingsPage;
+export default Listings;
