56a77d679a54ebf42691f473bca86327c38b60dd
Moorthi <moorthi.s@weblogix.com.au>
2025-08-13 15:50:19 +0530

    Home Page UI Update

--- src/app/footer/Footer.tsx
+++ src/app/footer/Footer.tsx
@@ -2,7 +2,7 @@
 
 import './footer.css'
 import Link from 'next/link'
-import { FaFacebookF, FaInstagram, FaYoutube, FaLinkedin } from 'react-icons/fa'
+import { FaFacebookF, FaInstagram, FaYoutube, FaLinkedin, FaPinterestP } from 'react-icons/fa'
 import { BsChevronUp } from 'react-icons/bs'
 
 const Footer = () => {
@@ -13,8 +13,62 @@ const Footer = () => {
     <>
       <footer className="style-8">
         <div className="container">
-          <div className="foot py-4 border-1 border-top brd-gray text-center">
-            <div className="content">
+          <div className="foot py-4 border-1 border-top brd-gray">
+            <div className="row">
+              {/* Left Column */}
+              <div className="col-lg-6">
+                <p>
+                  Caravan Marketplace (Web Logix) ABN 92 009 784 881. <br />
+                  Copyright © {currentYear}. All Rights Reserved.
+                </p>
+                <div className="disclaimer" style={{ marginTop: '12px' }}>
+                  <p>
+                    Disclaimer: Caravan marketplace is not affiliated with any manufacturers, dealers listed on our
+                    website. All product data listed on our website including logos, and brands are property of their
+                    respective owners. Product information is provided for informational purposes only.<br />
+                    Caravan marketplace does not make any warranty as to the accuracy, completeness or reliability of
+                    the information or accept any liability arising in any way from any omissions or errors. The
+                    information should not be regarded as advice or relied upon by you or any other person and we
+                    recommend that you seek professional advice before making any purchase decisions.
+                  </p>
+                </div>
+              </div>
+
+              {/* About Us Column */}
+              <div className="col-lg-3">
+                <h6 className="foot-title foot_xs">About Us</h6>
+                <ul className="menu footer_xs">
+                  <li><Link href={`${siteUrl}/caravan-dealers/`}>Caravan Dealers</Link></li>
+                  <li><Link href={`${siteUrl}/listings/`}>For Sale</Link></li>
+                  <li><Link href={`${siteUrl}/caravan-manufacturers/all/`}>Manufacturer Range</Link></li>
+                  <li><Link href={`${siteUrl}/blog/`}>Blog</Link></li>
+                  <li><Link href={`${siteUrl}/terms-conditions/`}>Terms & Conditions</Link></li>
+                  <li><Link href={`${siteUrl}/privacy-policy/`}>Privacy Policy</Link></li>
+                  <li><Link href={`${siteUrl}/privacy-collection-statement/`}>Privacy Collection Statement</Link></li>
+                  <li className="hidden-xs"><Link href={`${siteUrl}/about-us/`}>About</Link></li>
+                  <li className="hidden-lg hidden-md hidden-sm"><Link href={`${siteUrl}/about-us/`}>About Caravan Marketplace</Link></li>
+                  <li><Link href={`${siteUrl}/contact/`}>Contact Us</Link></li>
+                </ul>
+              </div>
+
+              {/* Popular Pages Column */}
+              <div className="col-lg-3">
+                <h6 className="foot-title foot_xs">Popular Pages</h6>
+                <ul className="menu footer_xs">
+                  <li><Link href={`${siteUrl}/best-caravans-full-off-road-capabilities-australia/`}>Best Off Road Caravans</Link></li>
+                  <li><Link href={`${siteUrl}/off-road-caravan-manufacturers/`}>Best Off Road Caravan Manufacturers</Link></li>
+                  <li><Link href={`${siteUrl}/caravan-manufacturers/`}>Best Caravan Manufacturers</Link></li>
+                  <li><Link href={`${siteUrl}/best-semi-off-road-caravans-australia-guide/`}>Best Semi Off Road Caravans</Link></li>
+                  <li><Link href={`${siteUrl}/best-caravans-for-extreme-off-road-travel/`}>Best Extreme Off Road Caravans</Link></li>
+                  <li><Link href={`${siteUrl}/best-luxury-caravans-australia-highlights-features-reviews/`}>Best Luxury Caravans</Link></li>
+                  <li><Link href={`${siteUrl}/top-family-off-road-caravans-australia/`}>Best Family Caravans</Link></li>
+                  <li><Link href={`${siteUrl}/touring-caravans/`}>Best Touring Caravans</Link></li>
+                </ul>
+              </div>
+            </div>
+
+            {/* Social Icons */}
+            <div className="content mt-3">
               <div className="foot-info logo-social">
                 <div className="socials">
                   <a href="https://www.facebook.com/caravansforsale.com.au" className="facebook" target="_blank" rel="noopener noreferrer">
@@ -31,45 +85,21 @@ const Footer = () => {
                   <a href="https://www.youtube.com/@caravansforsalecomau" className="youtube" target="_blank" rel="noopener noreferrer">
                     <FaYoutube />
                   </a>
-                  <a href="https://www.linkedin.com/company/caravansforsale/" className="linkedin" target="_blank" rel="noopener noreferrer">
-                    <FaLinkedin />
+                  <a href="https://au.pinterest.com/caravansforsalecomau/" className="pinterest" target="_blank" rel="noopener noreferrer">
+                    <FaPinterestP />
                   </a>
                 </div>
               </div>
-            </div>
-
-            <h6 className="foot-title foot_xs hidden-lg hidden-md hidden-sm">About Us</h6>
-            <ul className="menu footer_xs">
-              <li><Link href={`${siteUrl}/caravan-dealers/`}>Caravan Dealers</Link></li>
-              <li><Link href={`${siteUrl}/listings/`}>For Sale</Link></li>
-              <li><Link href={`${siteUrl}/caravan-manufacturers/all/`}>Manufacturer Range</Link></li>
-              <li><Link href={`${siteUrl}/blog/`}>Blog</Link></li>
-              <li><Link href={`${siteUrl}/terms-conditions/`}>Terms & Conditions</Link></li>
-              <li><Link href={`${siteUrl}/privacy-policy/`}>Privacy Policy</Link></li>
-              <li><Link href={`${siteUrl}/privacy-collection-statement/`}>Privacy Collection Statement</Link></li>
-              <li className="hidden-xs"><Link href={`${siteUrl}/about-us/`}>About</Link></li>
-              <li className="hidden-lg hidden-md hidden-sm"><Link href={`${siteUrl}/about-us/`}>About Caravan Marketplace</Link></li>
-              <li><Link href={`${siteUrl}/contact/`}>Contact Us</Link></li>
-            </ul>
-
-            <p>
-              Caravan Marketplace (Web Logix) ABN 92 009 784 881 | Copyright © {currentYear}. All Rights Reserved.
-            </p>
-
-            <div className="disclaimer" style={{ marginTop: '12px' }}>
-              <p>
-                <small>
-                  Disclaimer: Caravan marketplace is not affiliated with any manufacturers, dealers listed on our website. All product data listed on our website including logos, and brands are property of their respective owners. Product information is provided for informational purposes only.<br />
-                  Caravan marketplace does not make any warranty as to the accuracy, completeness or reliability of the information or accept any liability arising in any way from any omissions or errors. The information should not be regarded as advice or relied upon by you or any other person and we recommend that you seek professional advice before making any purchase decisions.
-                </small>
-              </p>
             </div>
           </div>
         </div>
       </footer>
 
       {/* To Top Button */}
-      <a href="#" className="to_top bg-gray rounded-circle icon-40 d-inline-flex align-items-center justify-content-center show">
+      <a
+        href="#"
+        className="to_top bg-gray rounded-circle icon-40 d-inline-flex align-items-center justify-content-center show"
+      >
         <BsChevronUp className="fs-6 text-white" />
       </a>
     </>
